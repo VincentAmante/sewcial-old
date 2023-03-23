149a61b252477da6b5ac10a59cba90857d5427fd
NehanTariq <nehan.shujia@gmail.com>
2023-03-23 17:05:30 +0400

    Create TestViewNehan.vue

---
+++ src/views/testViews/TestViewNehan.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+    // Import of a speechbubble
+    import SpeechBubble from '@/components/SpeechBubble.vue';
+    import TemplateHoverCard from '@/components/TemplateHoverCard.vue';
+</script>
+
+<template>
+    <main>
+        <!-- Just test divs, don't mind -->
+        <div class="test">
+            <div class="test-wrapper">
+                <!-- A component with slots in action -->
+                <SpeechBubble>
+                    <h2>Hey!</h2>
+                    <p>This speech bubble adapts to anything</p>
+                    <p>Still needs some more tweaks though to adapt to all possible options 
+                        (placing of this marker on the left for example)</p>
+                </SpeechBubble>
+
+                <!-- Component with a prop, the one above uses a default -->
+                <SpeechBubble alignment="right">
+                    <h2>Also</h2>
+                    <p>Since it uses vue slots, it's fully capable of inserting any HTML content inside</p>
+                    <div>
+                        <img src="/images/logo-white.svg" alt="">
+                        <div class="caption">Look at that!</div>
+                    </div>
+                    <p>Though it seems I need to configure the spacing for text elements</p>
+                </SpeechBubble>
+                <p>-------------------------------------------------</p>
+                <TemplateHoverCard :image="'https://i.pinimg.com/originals/81/fb/e1/81fbe1c31719a0bd6258fec7a34ad20c.jpg'">
+                <template #text>
+                    <h1>ITEM NAME</h1>
+                    <h3>OWNER'S NAME</h3>
+                </template>
+                </TemplateHoverCard>
+                <TemplateHoverCard :image="'https://scratchandstitch.com/wp-content/uploads/2019/09/denim-luggage-tags-diy-scratchandstitch-360x444@2x.jpg'">
+                <template #text>
+                    <h1>ITEM NAME</h1>
+                    <h3>OWNER'S NAME</h3>
+                </template>
+                </TemplateHoverCard>
+            </div>
+
+        </div>
+    </main>
+</template>
+<style scoped lang="scss">
+    .test {
+        max-width: 100%;
+        @include flex-col;
+        align-items: center;
+        justify-content: center;
+        padding-block: 10vmin;
+
+        .test-wrapper {
+            max-width: 600px;
+            width: 50%;
+            @include flex-col;
+            align-items: center;
+            justify-content: center;
+            .icon{
+                margin:10px;
+            }
+        }
+    }
+
+</style>
