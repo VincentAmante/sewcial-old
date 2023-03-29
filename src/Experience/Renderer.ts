f00d56cfe7a0333a045df8ab257f82f813a53b86
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2023-03-29 16:49:34 +0400

    ThreeJS Tweaks

--- src/Experience/Renderer.ts
+++ src/Experience/Renderer.ts
@@ -47,7 +47,6 @@ export default class Renderer
 
     resize()
     {
-        
         this.instance.setSize(this.sizes.width, this.sizes.height)
         this.instance.setPixelRatio(Math.min(this.sizes.pixelRatio, 2))
     }
--- src/Experience/World/Environment.ts
+++ src/Experience/World/Environment.ts
@@ -17,7 +17,7 @@ export default class Environment {
 
     setSunlight() {
         // Adds hard lighting, creates shadows
-        this.sunLight = new THREE.DirectionalLight('#ffffff', 3)
+        this.sunLight = new THREE.DirectionalLight('#ffffff', 1)
         this.sunLight.castShadow = true
         this.sunLight.shadow.camera.far = 20
 
@@ -28,7 +28,7 @@ export default class Environment {
         this.scene.add(this.sunLight)
 
         // Adds soft light to everything, most affects shadow light
-        this.ambientLight = new THREE.AmbientLight('#FF9999', 1)
+        this.ambientLight = new THREE.AmbientLight('#FF9999', 1.5)
         this.scene.add(this.ambientLight)
     }
 }
