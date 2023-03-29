1e1f8815193b2f36c6b40c7196d682a47e3ace49
NehanTariq <nehan.shujia@gmail.com>
2023-03-29 21:06:31 +0400

    Create EventField.vue

---
+++ src/components/FormFields/EventField.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+    import { computed } from 'vue';
+    const props = defineProps({
+        modelValue: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        placeholder: {
+            type: String,
+            required: false
+        },
+        isRequired: {
+            type: Boolean,
+            default: true
+        },
+        type: {
+            type: String,
+            default: 'text'
+        }
+    })
+
+    const emit = defineEmits(['update:modelValue'])
+    const textInput = computed({
+        get(){
+            return props.modelValue
+        },
+        set(value){
+            emit('update:modelValue', value)
+        }
+    })
+</script>
+
+<template>
+    <label :for="name" class="error">
+        <p><slot>MISSING LABEL</slot><span v-if="isRequired">*</span></p>
+        <input :type="type" :name="name" id="" :placeholder="placeholder" v-model.lazy="textInput" :required="isRequired">
+    </label>
+</template>
+
+<style scoped lang="scss">
+    label {
+        @include flex-col;
+        gap: 0;
+        // color: $clr-secondary;
+        width: 100%;
+
+        p {
+            margin: 0;
+            padding-bottom: .25em;
+
+            span {
+                padding-left: .1em;
+            }
+        }
+
+        input {
+            border-radius: 10px;
+            border: 2px solid $clr-secondary;
+            background-color: white;
+            padding-block: .5em;
+            padding-inline: .25em;
+
+            &:focus {
+                outline: none;
+                // border: 4px solid $clr-secondary;
+            }
+        }
+    }
+</style>
