89ddbecf47798be3151e5ec09c2e4839e10a61c4
NehanTariq <nehan.shujia@gmail.com>
2023-03-30 10:45:57 +0400

    Create TiltedHeading.vue

---
+++ src/components/TiltedHeading.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+    // Variables passed to this component are props
+    const props = defineProps({
+        alignment: {
+            type: String,
+            default: 'left'
+        }
+    })
+</script>
+
+<!-- Template with defined slots -->
+<!-- Currently, content by default goes to the one without a name -->
+<template lang="">
+    <h2 class="heading" :class="alignment">
+        <slot></slot>
+    </h2>
+</template>
+
+<!-- Scoped Styling -->
+<style scoped lang="scss">
+    .heading {
+        // background-color: $clr-accent-1;
+        // color: $clr-primary;
+        // padding: 5px 10px 5px 10px;
+        // border-radius: 5px;
+        border-radius: 5px;
+        padding: 5px 10px 5px 10px;
+        color: $clr-primary;
+        width: fit-content;
+        position: relative;
+        max-width: 50ch;
+        word-wrap: break-word;
+        rotate: -10deg;
+        &.left {
+            align-self: flex-start;
+            background-color: $clr-accent-1;
+        }
+    }
+</style>
